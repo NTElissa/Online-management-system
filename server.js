98f21a073df4b71ceb931783c1f41d00598563b6
NTElissa <ntihindukaelissa77@gmail.com>
2023-03-26 17:29:17 +0200

    new update

--- server.js
+++ server.js
@@ -1,123 +1,122 @@
-const {sequelize, User, Blog} = require('./models');
-const express = require('express');
+const {sequelize, User}=require('./models');
+const express=require('express');
 
-const app = express();
-app.use(express.json());
+const app= express()
+app.use(express.json())
 
-// Create a new blog
-app.post('/blogs', async (req, res) => {
-    const {title, content, userId} = req.body;
+app.post('/users',async (req,res) =>{
+    const{firstName,lastName,email}=req.body;
    
-    try {
-        const newBlog = await Blog.create({title, content, userId});
+    try{
+       const newUser =await User.create({firstName,lastName,email});
    
         return res.status(201).json({
-            message: "blog created",
-            data: newBlog
-        });
+          message:"user created",
+          data:newUser
+            });
    
-    } catch (err) {
-        console.log("error "+err);
-        return res.status(500).json(err);
+    }catch(err){
+     console.log("error "+err);
+     return res.status(500).json(err)
     }
-});
-
-// Get all blogs
-app.get('/blogs', async (req, res) => {
+    
+   });
+  
+// Get all users
+app.get('/users', async (req, res) => {
     try {
-        const blogs = await Blog.findAll({
-            include: [User]
+        const users = await User.findAll({
+            include: []
         });
-
+  
         return res.status(200).json({
-            message: "blogs retrieved",
-            data: blogs
+            message: "Users retrieved",
+            data: users
         });
     } catch (err) {
         console.log("error "+err);
         return res.status(500).json(err);
     }
-});
+  });
+  
 
-// Get a blog by id
-app.get('/blogs/:id', async (req, res) => {
+// Get a user by id
+app.get('/users/:id', async (req, res) => {
     const id = req.params.id;
-
+  
     try {
-        const blog = await Blog.findOne({
-            where: {id},
-            include: [User]
-        });
-
-        if (!blog) {
-            return res.status(404).json({
-                message: "blog not found"
-            });
-        }
-
-        return res.status(200).json({
-            message: "blog retrieved",
-            data: blog
-        });
-    } catch (err) {
-        console.log("error "+err);
-        return res.status(500).json(err);
-    }
-});
-
-// Update a blog by id
-app.put('/blogs/:id', async (req, res) => {
-    const id = req.params.id;
-    const {title, content, userId} = req.body;
-
-    try {
-        const [numRowsUpdated, [updatedBlog]] = await Blog.update(
-            {title, content, userId},
-            {where: {id}, returning: true}
-        );
-
-        if (numRowsUpdated !== 1) {
-            return res.status(404).json({
-                message: "blog not found"
-            });
-        }
-
-        return res.status(200).json({
-            message: "blog updated",
-            data: updatedBlog
-        });
-    } catch (err) {
-        console.log("error "+err);
-        return res.status(500).json(err);
-    }
-});
-
-// Delete a blog by id
-app.delete('/blogs/:id', async (req, res) => {
-    const id = req.params.id;
-
-    try {
-        const numRowsDeleted = await Blog.destroy({
+        const user = await User.findOne({
             where: {id}
         });
-
-        if (numRowsDeleted !== 1) {
+  
+        if (!user) {
             return res.status(404).json({
-                message: "blog not found"
+                message: "User not found"
             });
         }
-
+  
         return res.status(200).json({
-            message: "blog deleted"
+            message: "User retrieved",
+            data: user
         });
     } catch (err) {
         console.log("error "+err);
         return res.status(500).json(err);
     }
+  });
+  
+// Get a user by id
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+  
+    try {
+        const user = await User.findOne({
+            where: {id}
+        });
+  
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+  
+        return res.status(200).json({
+            message: "User retrieved",
+            data: user
+        });
+    } catch (err) {
+        console.log("error "+err);
+        return res.status(500).json(err);
+    }
+  });
+  
+
+// Delete a blog by id
+app.delete('/users/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+      const numRowsDeleted = await User.destroy({
+          where: {id}
+      });
+
+      if (numRowsDeleted !== 1) {
+          return res.status(404).json({
+              message: "USER not found"
+          });
+      }
+
+      return res.status(200).json({
+          message: "USER deleted"
+      });
+  } catch (err) {
+      console.log("error "+err);
+      return res.status(500).json(err);
+  }
 });
 
 app.listen({port:3000}, async () => {
-    console.log('Server listening on http://localhost:3000');
-    await sequelize.authenticate();
-    console.log('Connected to db');
+  console.log('Server listening on http://localhost:3000');
+  await sequelize.authenticate();
+  console.log('Connected to db');
 });
